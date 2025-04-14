c9a3089a98b42c87de752d5a46b9b9a5f3551760
AEX <antonioaestrella@gmail.com>
2025-04-14 14:28:43 -0400

    ref: routes and controller

--- src/controllers/mocks.controller.js
+++ src/controllers/mocks.controller.js
@@ -2,42 +2,8 @@ import { usersService } from "../services/index.js";
 import { petsService } from "../services/index.js";
 import { generateUsers, generatePets } from "../utils/mocks.js";
 
-// GenerateData
-const generateMockingData = async (users = 0, pets = 0) => {
-  try {
-    const petsList = await generateMockingPets(pets);
-
-    const savedPets = await petsService.insert(petsList);
-
-    const usersList = await generateMockingUsers(users);
-
-    const savedUsers = await usersService.insert(usersList);
-
-    return {
-      users: savedUsers.length,
-      pets: savedPets.length,
-    };
-  } catch (error) {
-    console.error("Error generating mocks:", error);
-  }
-};
-
-const generateMockingUsers = async (users) => {
-  try {
-    const usersList = [];
-
-    for (let i = 0; i < Number(users); i++) {
-      const user = await generateUsers();
-      usersList.push(user);
-    }
-
-    return usersList;
-  } catch (error) {
-    console.error("Failed generating mocked users:", error);
-  }
-};
-
-const generateMockingPets = async (pets) => {
+const generateMockingData = async (req, res) => {
+  const { users = 0, pets = 0 } = req.params;
   try {
     const petsList = [];
 
@@ -46,9 +12,63 @@ const generateMockingPets = async (pets) => {
       petsList.push(pet);
     }
 
-    return petsList;
+    const savedPets = await petsService.insert(petsList);
+
+    const usersList = [];
+
+    for (let i = 0; i < Number(users); i++) {
+      const user = await generateUsers();
+      usersList.push(user);
+    }
+
+    const savedUsers = await usersService.insert(usersList);
+
+    const result = {
+      users: savedUsers.length,
+      pets: savedPets.length,
+    };
+
+    res.status(200).json({ status: "success", payload: result });
   } catch (error) {
-    console.error("Failed generating mocked pets:", error);
+    res
+      .status(500)
+      .json({ status: "error", code: 500, message: error.message });
+  }
+};
+
+const generateMockingUsers = async (req, res) => {
+  const { users } = req.params;
+  try {
+    const usersList = [];
+
+    for (let i = 0; i < Number(users); i++) {
+      const user = await generateUsers();
+      usersList.push(user);
+    }
+
+    res.status(200).json({ status: "success", payload: usersList });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", code: 500, message: error.message });
+  }
+};
+
+const generateMockingPets = async (req, res) => {
+  const { pets } = req.params;
+  try {
+    const petsList = [];
+
+    for (let i = 0; i < Number(pets); i++) {
+      const pet = await generatePets();
+      petsList.push(pet);
+    }
+
+    res.status(200).json({ status: "success", payload: petsList });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", code: 500, message: error.message });
   }
 };
 
--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -3,46 +3,8 @@ import mocksController from "../controllers/mocks.controller.js";
 
 const router = Router();
 
-router.post("/generatedata/:users/:pets", async (req, res) => {
-  try {
-    const { users, pets } = req.params;
-
-    const mocks = await mocksController.generateMockingData(users, pets);
-
-    res.status(200).json({ status: "success", payload: mocks });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ status: "error", code: 500, message: error.message });
-  }
-});
-
-router.get("/mockingusers/:users", async (req, res) => {
-  try {
-    const { users } = req.params;
-
-    const usersList = await mocksController.generateMockingUsers(users);
-
-    res.status(200).json({ status: "success", payload: usersList });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ status: "error", code: 500, message: error.message });
-  }
-});
-
-router.get("/mockingpets/:pets", async (req, res) => {
-  try {
-    const { pets } = req.params;
-
-    const petsList = await mocksController.generateMockingPets(pets);
-
-    res.status(200).json({ status: "success", payload: petsList });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ status: "error", code: 500, message: error.message });
-  }
-});
+router.post("/generatedata/:users/:pets", mocksController.generateMockingData);
+router.get("/mockingusers/:users", mocksController.generateMockingUsers);
+router.get("/mockingpets/:pets", mocksController.generateMockingPets);
 
 export default router;
