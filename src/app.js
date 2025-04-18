93be6b2e4d8fa400a801c8e98af86e2fdee36e26
AEX <antonioaestrella@gmail.com>
2025-04-18 16:16:59 -0400

    fea: compression

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import compression from 'express-compression';
 
 import env from './config/env.config.js';
+import ConnectMongoDB from './config/mongo.config.js';
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
@@ -10,16 +11,23 @@ import sessionsRouter from './routes/sessions.router.js';
 import mocksRouter from './routes/mocks.router.js';
 
 const app = express();
-const PORT = env.port || 8080;
-const connection = mongoose.connect(env.mongoUrl);
+const PORT = env.port;
 
+app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use('/api/users', usersRouter);
 app.use('/api/pets', petsRouter);
 app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/mocks', mocksRouter);
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+// Database connection
+ConnectMongoDB.getInstance();
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server up on port: http://localhost:${PORT}`);
+});
